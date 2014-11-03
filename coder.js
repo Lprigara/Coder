c24c297acbd7c3e61adc69e57cef8539be7090bd
Lprigara <lprigara@gmail.com>
2014-11-03 19:19:31 +0000

    Problem with eval(). Find out an alternative

--- coder.js
+++ coder.js
@@ -1,16 +1,44 @@
 $(document).ready(init);
 
 function changeHeight(){
-	var windowHeight=$(window).height()-160;
+	var windowHeight=$(window).height()-140;
 	$(".codeContainer").css("height", windowHeight+"px");
 }
 
 function clickOptionTopMenu(){
-	$(this).toggleClass("selected"); //add or remove one or more classes depending the class`s presence
-}
+	
 
+}
 function init(){
 	changeHeight();
-	$(".optionTopMenu").click(clickOptionTopMenu);
+	$(".optionTopMenu").click(function(){
+		$(this).toggleClass("pressed"); //add or remove one or more classes depending the class`s presence
+
+		//display in screen
+		var id=$(this).attr("name");
+		$("#"+id+"Container").toggle();
+
+		var number = $('.codeContainer').filter(function() {
+	 		return $(this).css('display') !== 'none';
+		}).length;
+
+		var width=100/number;
+		$(".codeContainer").css("width",width+"%");
+	});
+
+	$("#runButton").click(function(){
+		var htmlCode = $("#html").val();
+		var cssCode = $("#css").val();
+		var jsCode = $("#js").val();
+		$('#result').contents().find('html').html("<style>"+cssCode+"</style>"+htmlCode);
+		document.getElementById("result").contentWindow;.eval(jsCode);
+	});
 }
 
+
+
+ 	 	
+ 
+ 	 	 	
+ 
+ 
